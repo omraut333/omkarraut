d841e2526fdbe98fb9a573cbd41857eb9e7d3048
omraut333 <79253721+omraut333@users.noreply.github.com>
2021-02-20 12:03:11 +0530

    Add files via upload

--- omkarraut/settings.py
+++ omkarraut/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -132,3 +132,5 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+django_heroku.settings(locals())
